97fafc34b2cc3fcf23f39dc73cb72a9ab370ac4f
papa31 <visidoor@yandex.ru>
2020-07-15 16:11:12 +0500

    Specify data requirements (change Task.js)

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Task({
   task: { id, title, state },
@@ -35,4 +36,14 @@ export default function Task({
       </div>
     </div>
   )
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 }
